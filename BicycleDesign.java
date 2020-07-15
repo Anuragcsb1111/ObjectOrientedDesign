c75c3ad04b05e1ee38a0cba36d3a2914db4da39f
Anurag <sanurag616@gmail.com>
2020-07-15 07:42:25 +0530

    added wheel class seperately preserving the single responsibility of gears class

--- BicycleDesign.java
+++ BicycleDesign.java
@@ -5,32 +5,62 @@ class Gears
 {
 	private int chainring;
 	private int cog;
-	//private int wheel;
+	private int wheel;
 
 	Gears(int chainring , int cog)
 	{
 		this.chainring  = chainring;
 		this.cog = cog;
 	}
-	/*
 
 	Gears(int chainring , int cog, int wheel)
 	{
 		this.chainring  = chainring;
 		this.cog = cog;
 		this.wheel = wheel;
-	}*/
+	}
 
 	public double ratio()
 	{
 		return chainring/cog;
 	}
 
-/*
 	public double gear_inches()
 	{
 		return 
-	}*/
+	}
 }
 
 
+class Wheel
+{
+	private int rim;
+	private int tire;
+
+	Wheel(int rim, int tire)
+	{
+		this.rim = rim;
+		this.tire = tire;
+	}
+
+	public int diameter()
+	{
+		return rim + (tire*2);
+	}
+
+	public double circumference()
+	{
+		int d = diameter();
+		return d * Math.PI;
+	}
+}
+
+public class BicycleDesign
+{
+	public static void main (String [] args)
+	{
+		Wheel wheel = new Wheel(26,1.5);
+
+		System.out.println(wheel.circumference());
+	}
+}
